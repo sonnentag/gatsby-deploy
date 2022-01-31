34ef5b0c72f041f9137492cfa1a816238cc187ca
Tony Sims <asims@oomphinc.com>
2022-01-31 03:42:26 -0600

    data json nodes done

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const MainPage = ({ location }) => (
-  <Layout>
+  <>
     <Seo title="Main" />
     <h1>Hi from the second page</h1>
     <p>Welcome to {location.state?.query && location.state.query} </p>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </>
 )
 
 export default MainPage
