f2fb1beda2862bd64f9d994b11987a1596f14594
Tony Sims <asims@oomphinc.com>
2022-02-02 06:48:10 -0600

    adding query lists to main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,57 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Seo from "../components/seo"
 
-const MainPage = ({ location }) => (
+
+const MainPage = ({ location, data }) => (
   <>
     <Seo title="Main" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to {location.state?.query && location.state.query} </p>
+    <h1>Welcome to {location.state?.query && location.state.query} </h1>
+    <ul>
+    {location.state?.query && location.state.query === 'repos'
+        ? data.repos.edges.map(edge => (
+            <li key={edge.node.parent.name}>
+                {edge.node.parent.name}
+            </li>
+        ))
+        : location.state?.query && location.state.query === 'packages'
+        ? data.packages.distinct.map(node => (
+            <li key={node}>
+                {node}
+            </li>
+        ))
+        : <li/> 
+    }
+    </ul>
     <Link to="/">Go back to the homepage</Link>
   </>
 )
+
+export const mainQuery = graphql`
+  query {
+    packages: allDataJson {
+      edges {
+        node {
+          packages {
+            package
+          }
+        }
+      }
+      distinct(field: packages___package)
+    }
+    repos: allDataJson {
+      edges {
+        node {
+          parent {
+            ... on File {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default MainPage
